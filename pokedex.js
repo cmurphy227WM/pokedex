ca31447419d3cae1a00b927448d93ab401cd04f9
Marcus Tinney <mtinney191@west-mec.org>
2020-10-20 18:31:37 -0700

    Day 2, Got things displayed finally

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,12 @@
+//Capitalization
+
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+
+
 ////////////////Use Dom to select get and post button/////////////
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
@@ -26,16 +35,21 @@ const sendHttpRequest = (method,url) =>{
         var ans = pad.substring(0, pad.length - str.length - str.length) + str
 
 //////// Assign diferent parts to pokedex using dom selection/////////
-        document.getElementById('info_id').value = "Pokedex Number: #" + ans;
-        document.getElementById('info_stats').value = data.types[0].type.name;
-        document.getElementById('info_height').value = data.height+" ft";
-        document.getElementById('info_weight').value = data.weight+' lbs';
-        document.getElementById('info_moves').innerHTML = "Possible Moves:";
-        document.getElementById('info_name').innerHTML = data.name;
+        document.getElementById('info_id').innerHTML = "Pokedex Number: #" + ans;
+        document.getElementById('info_stats').innerHTML = "Type: " + capitalize(data.types[0].type.name);
+        document.getElementById('info_name').innerHTML = capitalize(data.name);
         document.getElementById('pokedex_screen_shiny').src = data.sprites.front_shiny;
-        for(var x=0; x < data.moves.length;x++)
-        {
-            document.getElementById('info_moves').value += data.moves[x].move.name+",";
+        document.getElementById('info_abilities').innerHTML = "Ability: " + capitalize(data.abilities[0].ability.name);
+        if (data.abilities[1].ability.is_hidden = true) {
+            document.getElementById('info_abilities').innerHTML = "Ability: " + capitalize(data.abilities[0].ability.name) + "<br><br>Hidden Ability: " + capitalize(data.abilities[1].ability.name);
+        } else if (data.abilities[2].ability.is_hidden = true) {
+            document.getElementById('info_abilities').innerHTML = "Abilities: " + capitalize(data.abilities[0].ability.name) + " & " + capitalize(data.abilities[1].ability.name) + "<br><br>Hidden Ability: " + capitalize(data.abilities[2].ability.name);
+        }
+
+        if (data.abilities[2].ability.is_hidden = true) {
+            document.getElementById('info_abilities').innerHTML = "Abilities: " + capitalize(data.abilities[0].ability.name) + " & " + capitalize(data.abilities[1].ability.name) + "<br><br>Hidden Ability: " + capitalize(data.abilities[2].ability.name);
+        } else if (data.abilities[1].ability.is_hidden = true) {
+            document.getElementById('info_abilities').innerHTML = "Ability: " + capitalize(data.abilities[0].ability.name) + "<br><br>Hidden Ability: " + capitalize(data.abilities[1].ability.name);
         }
     }
 
@@ -49,6 +63,5 @@ const sendHttpRequest = (method,url) =>{
 /// add event listener to get button ///
 
 getBtn.addEventListener('click', getData);
-
 
 
