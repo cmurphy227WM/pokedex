169b87944ede84541d8af4aa6c4105a342c56f05
Connor Murphy <cmurphy227@west-mec.org>
2020-10-21 16:53:45 -0700

    Types JS

---
+++ types.js
@@ -0,0 +1,51 @@
+//Capitalization
+
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+
+
+////////////////Use Dom to select get and post button/////////////
+const getBtn = document.getElementById('get-btn');
+
+///////create callback function thats used when button clicked////////
+const getData = () =>{
+    var answer = document.getElementById('pName').value;
+    console.log(answer);
+    var tops ="https://pokeapi.co/api/v2/type/" + answer;
+    sendHttpRequest('GET', tops);
+};
+if(answer == physical || answer == 1){
+    document.getElementById('typeImg').src = "types/Fighting.png";
+}
+////////// Create callback function with embeded promise/////////////
+const sendHttpRequest = (method,url) =>{
+    const promise = new Promise((resolve, reject) =>{
+    console.log(url);
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = () => {
+///////////// When browser load parse http req to usable format//////////
+        const data = JSON.parse(xhr.response);//converts JSON data to RAW JS Objs & dataTypes
+        console.log(data);//JSON Data
+
+        document.getElementById('typeImg').src = data.sprites.front_default;
+        var str = "" + data.id
+        var pad = "000"
+        var ans = pad.substring(0, pad.length - str.length - str.length) + str
+
+//////// Assign diferent parts to pokedex using dom selection/////////
+    }
+
+    // or xhr.addeventlister('load',function)
+    xhr.send();
+    })
+
+};
+
+
+/// add event listener to get button ///
+
+getBtn.addEventListener('click', getData);
